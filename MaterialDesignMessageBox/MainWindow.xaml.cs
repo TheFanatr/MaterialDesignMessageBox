deba1267850025d2d7e084fae6c586b60a1a4a94
SMUSFTC <SMUSFTCTeam1>
2017-02-27 15:55:03 -0800

    Set up enums and public variables for window initialization.

---
+++ MaterialDesignMessageBox/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace MaterialDesignMessageBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MDMessageBox : Window
+    {
+        public string Message { get; set; }
+        UIElement MessageTypeSymbol { get; set; }
+        public enum MessageTypes
+        {
+            Error,
+            Info,
+            Alert,
+            Critical
+        }
+        public MessageTypes MessageType
+        {
+            get { return MessageType; }
+            set
+            {
+                switch (MessageType)
+                {
+                    case MessageTypes.Alert:
+                        MessageTypeSymbol = new PackIcon
+                        { Kind = PackIconKind.AlertCircle };
+                        break;
+                    case MessageTypes.Info:
+                        MessageTypeSymbol = new PackIcon
+                        { Kind = PackIconKind.Information };
+                        break;
+                    case MessageTypes.Error:
+                        MessageTypeSymbol = new PackIcon
+                        { Kind = PackIconKind.CloseCircle };
+                        break;
+                    case MessageTypes.Critical:
+                        MessageTypeSymbol = new PackIcon
+                        { Kind = PackIconKind.Exclamation };
+                        break;
+                }
+                return;
+            }
+        }
+        public MDMessageBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MaterialDesignMessageBox/Unit Test.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MaterialDesignMessageBox
+{
+    /// <summary>
+    /// Interaction logic for Unit_Test.xaml
+    /// </summary>
+    public partial class Unit_Test : Window
+    {
+        public Unit_Test()
+        {
+            InitializeComponent();
+            Button testButton = new Button { Content = "Test MDMessageBox" };
+            // TODO: Check that the variables in MDMessageBox can be set through initializing the MDMessageBox with the "curly brackets method".
+            new MDMessageBox()
+            {
+                Message = "happy",
+                message
+            };
+
+            testButton.Click += (s, e) => { };
+            grid.Children.Add(testButton);
+        }
+    }
+}
